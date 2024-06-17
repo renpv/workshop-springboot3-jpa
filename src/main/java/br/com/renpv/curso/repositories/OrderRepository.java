87668d656a78c5d57502ec26059a1ef3dd9e64bf
Renato <renatopaiva@unilab.edu.br>
2024-06-16 21:51:51 -0300

    Order, Instant, ISO 8601

---
+++ src/main/java/br/com/renpv/curso/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package br.com.renpv.curso.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.renpv.curso.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long>{
+
+}
